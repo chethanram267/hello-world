8540c99b1fb385704517e828c663eef039b23e44
chethanram267 <153764836+chethanram267@users.noreply.github.com>
2024-02-17 14:23:38 +0530

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_aYjaYVLJv', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '3qqjo96egfkatd4o90sm2gp4cp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_poRrhbP4w', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '16eg0ardcukdo8vacml69i76', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
